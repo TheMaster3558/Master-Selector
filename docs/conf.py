e441642643f1f7f739fa07a7a9dadb355b181898
chawkk6404 <chawk_k@outlook.com>
2024-02-09 22:34:05 -0800

    Remove logo

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'shibuya'
 html_static_path = ['_static']
 
-html_logo = '_static/logo.png'
 
 html_theme_options = {
     'github_url': 'https://github.com/TheMaster3558/Master-Selector',
